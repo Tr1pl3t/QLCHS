a3ee16e8a6b156814faa52ee8d1576d1e8c5071f
pengunts <sontungpham123@gmail.com>
2025-09-28 20:29:54 +0700

    test

---
+++ QLCHS.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.qlchs;
+
+/**
+ *
+ * @author ASUS
+ */
+public class QLCHS {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
