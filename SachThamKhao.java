4311b506da69654d96f9c7f08424617bea790032
Tan Tung <tantung2310@gmail.com>
2025-10-03 20:43:14 +0700

    Add files via upload

---
+++ SachThamKhao.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+public class SachThamKhao extends Sach {
+    private String linhVuc;
+    private String trinhDo;
+
+    public SachThamKhao() {
+    }
+
+    public SachThamKhao(String maSach, String tenSach, String theLoai, int namXuatBan, int donGiaBan, int soLuong,
+            String linhVuc, String trinhDo) {
+        super(maSach, tenSach, theLoai, namXuatBan, donGiaBan, soLuong);
+        this.linhVuc = linhVuc;
+        this.trinhDo = trinhDo;
+    }
+
+    public SachThamKhao(SachThamKhao sgk) {
+        super((Sach) sgk);
+        this.linhVuc = sgk.linhVuc;
+        this.trinhDo = sgk.trinhDo;
+    }
+
+    public String getLinhVuc() {
+        return linhVuc;
+    }
+
+    public void setLinhVuc(String linhVuc) {
+        this.linhVuc = linhVuc;
+    }
+
+    public String getTrinhDo() {
+        return trinhDo;
+    }
+
+    public void setTrinhDo(String trinhDo) {
+        this.trinhDo = trinhDo;
+    }
+
+    @Override
+    public void nhapSach() {
+        Scanner sc = new Scanner(System.in);
+        super.nhapSach();
+        System.out.print("Nhap linh vuc: ");
+        linhVuc = sc.nextLine();
+        System.out.print("Nhap trinh do: ");
+        trinhDo = sc.nextLine();
+    }
+
+    @Override
+    public void xuatSach() {
+        super.xuatSach();
+        System.out.printf(" %-5s %-10s %-10s %-10s\n", "-", "-", linhVuc, trinhDo);
+    }
+}
